d662b65e0f4c31ef53cbdc32de4d4972814622d3
Ryan Kwon <kznm.develop@gmail.com>
2023-04-21 14:51:54 +0900

    Update merge_results.py

--- merge_results.py
+++ merge_results.py
@@ -4,4 +4,4 @@ data1 = pd.read_csv('./results_time.csv')
 data2 = pd.read_csv('./results_mem.csv')
 
 data3 = pd.merge(data1, data2)
-data3.to_csv('full.csv', index=False)
+data3.to_csv('result.csv', index=False)
