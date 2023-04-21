24926d4aec8eca54b93c4042c8f50f3f182a7bbb
shißdo <61821471+shiueo@users.noreply.github.com>
2023-04-21 15:53:31 +0900

    Update complexity_trends.py

--- complexity_trends.py
+++ complexity_trends.py
@@ -7,13 +7,17 @@ import matplotlib.pyplot as plt
 from sorting.aps import adaptive_partition_sort
 from sorting.introsort import introsort
 from sorting.timsort import timsort
+from sorting.quicksort import quick_sort
 
 aps_t, aps_m = [], []
 timsort_t, timsort_m = [], []
 introsort_t, introsort_m = [], []
+quick_sort_t, quick_sort_m = [], []
+
+N = 5000
 
 # time
-for i in range(1, 5001):
+for i in range(1, N+1):
     rand = [random.randint(0, i) for _ in range(i)]
 
     start = time.time()
@@ -31,18 +35,24 @@ for i in range(1, 5001):
     end = time.time()
     introsort_t.append((end - start) * 1000)
 
+    start = time.time()
+    arr = quick_sort(rand)
+    end = time.time()
+    quick_sort_t.append((end - start) * 1000)
+
     if i % 100 == 0:
-        print(i, aps_t[-1], timsort_t[-1], introsort_t[-1])
+        print(i, aps_t[-1], timsort_t[-1], introsort_t[-1], quick_sort_t[-1])
 
 plt.plot(aps_t)
 plt.plot(timsort_t)
 plt.plot(introsort_t)
-plt.legend(['APS', 'Timsort', 'Introsort'])
+plt.plot(quick_sort_t)
+plt.legend(['APS', 'Timsort', 'Introsort', 'quick_sort'])
 plt.title('Time Complexity')
 plt.show()
 
 # memory
-for i in range(1, 5001):
+for i in range(1, N+1):
     rand = [random.randint(0, i) for _ in range(i)]
 
     tracemalloc.start()
@@ -63,12 +73,19 @@ for i in range(1, 5001):
     introsort_m.append(peak)
     tracemalloc.stop()
 
+    tracemalloc.start()
+    arr = quick_sort(rand)
+    current, peak = tracemalloc.get_traced_memory()
+    quick_sort_m.append(peak)
+    tracemalloc.stop()
+
     if i % 100 == 0:
-        print(i, aps_m[-1], timsort_m[-1], introsort_m[-1])
+        print(i, aps_m[-1], timsort_m[-1], introsort_m[-1], quick_sort_m[-1])
 
 plt.plot(aps_m)
 plt.plot(timsort_m)
 plt.plot(introsort_m)
-plt.legend(['APS', 'Timsort', 'Introsort'])
+plt.plot(quick_sort_m)
+plt.legend(['APS', 'Timsort', 'Introsort', 'quicksort'])
 plt.title('Memory Complexity')
 plt.show()
